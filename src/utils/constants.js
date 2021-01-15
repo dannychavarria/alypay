0ce5eae12c62774879d62344ec8a7af132767044
msobalvarro <mssrsobalvarro1997@gmail.com>
2021-01-15 14:31:51 -0600

    testing before prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -82,8 +82,8 @@ export const Colors = {
 const PORT = "3000"
 
 /**Direction for server */
-// export const serverAddress = "https://alypay.uc.r.appspot.com"
-// export const serverAddress = "https://root-anvil-299019.uc.r.appspot.com"
+export const serverAddress = "https://alypay.uc.r.appspot.com"
+// export const serverAddress = "https://test-after-prod.uc.r.appspot.com"
 //export const serverAddress = "https://192.168.1.224:3000"
 export const serverAddress = Platform.OS === "ios" ? `http://localhost:${PORT}` : `http://192.168.0.132:${PORT}`
 
