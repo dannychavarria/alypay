aa91371bf14fdc36e004fd9233f8ae95723f2ef4
Samuel Mendoza <slmacajina@hotmail.com>
2021-05-13 14:44:13 -0600

    conflixtos arreglados

--- src/Services/Kyc/SubmintInfoKyc.js
+++ src/Services/Kyc/SubmintInfoKyc.js
@@ -6,33 +6,27 @@ import {
     loader,
 } from "../../utils/constants"
 import store from "../../store/index"
-import { useNavigation } from "@react-navigation/native"
 
 export default async function submitInfo(dataSent) {
     try {
         loader(true)
 
-        navigation.push("Main")
+        const {navigation} = store.getState()
 
-        // console.log("DataSent: ", dataSent)
+        const { data: response } = await http.post(
+            "/kyc",
+            dataSent,
+            getHeadersMultipartFormData(),
+        )
 
-        // const {navigation} = store.getState()
-
-        // const { data: response } = await http.post(
-        //     "/kyc",
-        //     dataSent,
-        //     getHeadersMultipartFormData(),
-        // )
-
-        // console.log(response)
-
-        // if (response.error) {
-        //     throw String(response.message)
-        // } else if (response.response === "success") {
-        //     successMessage("Tu registro esta en proceso de aceptacion")
-        //     navigation.push("Main")
-        // }
+        if (response.error) {
+            throw String(response.message)
+        } else {
+            successMessage(response.message)
+            navigation.pop()
+        }
     } catch (error) {
+        console.log(error.toString())
         errorMessage(error.toString())
     } finally {
         loader(false)
--- src/components/ModalKyc/ModalKyc.js
+++ src/components/ModalKyc/ModalKyc.js
@@ -19,12 +19,12 @@ import LogoFunko from "../../static/AlyFunko.png"
 // Import Stoore
 import store from "../../store/index"
 
-const ModalKyc = _ => {
+const ModalKyc = ({kycConfirm}) => {
     const classes = useStyles(ModalKycStyles)
 
     const { navigation } = store.getState()
 
-    const [showModal, setShowModal] = useState(true)
+    const [showModal, setShowModal] = useState(false)
 
     const onKyc = () => {
         setShowModal(false)
@@ -57,23 +57,31 @@ const ModalKyc = _ => {
         }
     }
 
-    useEffect(() => {
-        store.subscribe(() => {
-            const newStore = store.getState().global
+    useEffect(()=>{
+        console.log('entra')
+        setShowModal(kycConfirm == 1 && showModal == false ? false : true)
+    },[])
+
+    // useEffect(() => {
+    //     store.subscribe(() => {
+    //         const newStore = store.getState().global
+
+    //         console.log("NewStore",newStore)
 
     //         const conditional =
     //             "kyc_type" in newStore && newStore?.kyc_type === 0
 
-            console.log(conditional)
+    //         console.log('Condicional',conditional)
 
-            setShowModal(conditional)
-        })
-    }, [])
+    //         setShowModal(conditional)
+    //     })
+    // }, [])
+
+    console.log('showModal: ', showModal)
 
     return (
         <Modal
             isVisible={showModal}
-            onBackdropPress={_ => setShowModal(false)}
             animationIn="fadeIn"
             animationOut="fadeOut">
             <View style={classes.container}>
